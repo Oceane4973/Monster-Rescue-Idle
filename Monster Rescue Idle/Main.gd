203d440cf459729e5bbcc0c30ec18144efeeaf49
InaneInaccompli <92868301+InaneInaccompli@users.noreply.github.com>
2024-03-29 10:33:29 +0100

    Légère modification du script

--- Monster Rescue Idle/Main.gd
+++ Monster Rescue Idle/Main.gd
@@ -1,5 +1,6 @@
 extends Area3D
 
+const move_speed := 4.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,5 +12,4 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
-	const move_speed := 4.0
 	$CheminVisiteurs/SuiviChemin.progress += move_speed * delta
