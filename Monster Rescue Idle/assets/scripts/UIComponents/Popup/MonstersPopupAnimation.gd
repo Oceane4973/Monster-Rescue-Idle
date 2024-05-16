ea223b2d5e0b19123868be2a1e2214fda343bebb
Océane MONGES <oceane@bebou.oceanemonges.com>
2024-05-16 10:27:53 +0200

    create list of monster with array

--- Monster Rescue Idle/assets/scripts/UIComponents/Popup/MonstersPopupAnimation.gd
+++ Monster Rescue Idle/assets/scripts/UIComponents/Popup/MonstersPopupAnimation.gd
@@ -1,5 +1,19 @@
 extends Control
 
+@onready var monster_list = get_node("PanelContainer/VBoxContainer/ScrollContainer/MonsterList")
+var MonsterItemScene = preload("res://assets/scenes/UIComponents/Items/Monsters/MonsterItem.tscn")
+
+var list_Of_Monsters = [
+	Monster.new(),
+	Monster.new()
+]
+
+func _ready():
+	for monster in list_Of_Monsters :
+		var instance = MonsterItemScene.instantiate()
+		instance.load_monster_class(monster)
+		monster_list.add_child(instance)
+	
 func _on_close_monsters_popup_pressed():
 	$MonstersPopupAnimation.play_backwards("popUP")
 
